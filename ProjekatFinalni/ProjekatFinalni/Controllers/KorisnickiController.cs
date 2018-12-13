185e17514b64cd6c4149d43f81de819256efa87b
Aleksandar Vukovic <akidelija1998@gmail.com>
2018-12-13 17:16:53 +0100

    Ubacivanje korisnika

---
+++ ProjekatFinalni/ProjekatFinalni/Controllers/KorisnickiController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjekatFinalni.Models;
+
+namespace ProjekatFinalni.Controllers
+{
+    public class KorisnickiController : Controller
+    {
+        // GET: Korisnicki
+        Korisnik kmodel = new Korisnik();
+        public ActionResult DodajiliIzmeni( int id=0)
+        {
+            return View(kmodel);
+        }
+    }
+}
